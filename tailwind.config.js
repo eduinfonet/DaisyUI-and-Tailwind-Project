b7a38019d61ea6a4a4ee8eaaa9774774c38922a7
eduinfosite <frogjump721@gmail.com>
2024-01-19 23:00:08 +0600

    color change

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,22 @@ module.exports = {
   },
   plugins: [],
 };
+
+module.exports = {
+  //...
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#a991f7",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+};
